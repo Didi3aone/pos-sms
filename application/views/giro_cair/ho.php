3fe76742b92c68af5517283b51807209c7b0ff73
fredynepra <fredy.nepra@gmail.com>
2017-12-04 22:22:08 +0700

    pos-sms

--- application/views/giro_cair/ho.php
+++
@@ -1,4 +0,0 @@
-<form name="form1" id="form1" method="post" action="<?=base_url()?>GiroCair">
-Nama:<input type="text" name="nama"/>
-Kelas:<input type="text" name="kelas" onkeydown="if(event.keyCode == 13){document.getElementById('form1').submit();}/">
-</form>
